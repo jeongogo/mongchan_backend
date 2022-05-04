759f3dde0266533660f41bfcecbcd437df752194
jeongogo <chcgogo2@naver.com>
2022-05-04 11:10:04 +0900

    edit

--- src/api/run/index.js
+++ src/api/run/index.js
@@ -4,5 +4,7 @@ import * as runCtrl from './run.ctrl';
 const run = new Router();
 
 run.get('/', runCtrl.list);
+run.get('/edit', runCtrl.listBySort);
+run.post('/edit/:id', runCtrl.update);
 
 export default run;
--- src/api/run/run.ctrl.js
+++ src/api/run/run.ctrl.js
@@ -2,8 +2,16 @@ import Run from '../../models/run';
 
 export const list = async (ctx) => {
   try {
-    const data = await Run.find();
-    console.log(data)
+    const data = await Run.find().sort({name: -1});
+    ctx.body = data;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const listBySort = async (ctx) => {
+  try {
+    const data = await Run.find().sort({name: 1});
     ctx.body = data;
   } catch (e) {
     ctx.throw(500, e);
@@ -12,6 +20,7 @@ export const list = async (ctx) => {
 
 export const update = async (ctx) => {
   const { id } = ctx.params;
+
   try {
     const data = await Run.findByIdAndUpdate(id, ctx.request.body, {
       new: true,
