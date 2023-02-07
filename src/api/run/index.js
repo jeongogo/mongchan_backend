2148cd99416b4eb7a2c5015d2bb1ee87491c1a56
hongchan <chcgogo1357@gmail.com>
2023-02-07 10:26:45 +0900

    팀 가산점

--- src/api/run/index.js
+++ src/api/run/index.js
@@ -6,5 +6,7 @@ const run = new Router();
 run.get('/', runCtrl.list);
 run.get('/edit', runCtrl.listBySort);
 run.post('/edit/:id', runCtrl.update);
+run.get('/team', runCtrl.getTeam);
+run.post('/team', runCtrl.updateTeam);
 
 export default run;
--- src/api/run/run.ctrl.js
+++ src/api/run/run.ctrl.js
@@ -1,4 +1,5 @@
 import Run from '../../models/run';
+import Team from '../../models/team';
 
 export const list = async (ctx) => {
   try {
@@ -21,9 +22,36 @@ export const listBySort = async (ctx) => {
 export const update = async (ctx) => {
   const { id } = ctx.params;
   const keyName = 'week' + ctx.request.body.week;
-
+  
   try {
     const data = await Run.findByIdAndUpdate(id, { [keyName]: ctx.request.body.data }, {
+      new: true,
+    }).exec();
+    if (!data) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = data;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const getTeam = async (ctx) => {
+  try {
+    const data = await Team.findOne();
+    ctx.body = data;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const updateTeam = async (ctx) => {
+  const id = "63e19e998c1174fad9716ef6";
+  const keyName = 'week' + ctx.request.body.week;
+  
+  try {
+    const data = await Team.findByIdAndUpdate(id, { [keyName]: ctx.request.body.data }, {
       new: true,
     }).exec();
     if (!data) {
